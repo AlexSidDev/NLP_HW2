065ceb6266aa5887f6345b7ed77fe59c429bafeb
Александр <alexsiid29@gmail.com>
2024-12-12 21:56:12 +0300

    Added weighted loss experiments for Bert

--- src/nn/training_utils/train_engine.py
+++ src/nn/training_utils/train_engine.py
@@ -16,19 +16,25 @@ class Trainer:
                  val_dataloader,
                  device: str,
                  scheduler=None,
-                 use_dice=False):
+                 use_weighted=False):
         self.device = device
         self.model = model.to(device)
         self.train_dataloader = train_dataloader
         self.val_dataloader = val_dataloader
 
-        self.criterion = nn.CrossEntropyLoss(ignore_index=-100) if not use_dice else DiceLoss()
+        if use_weighted:
+            class_weights = torch.ones((9,), device=device)
+            class_weights[0] = 0.5
+            self.criterion = nn.CrossEntropyLoss(weight=class_weights, ignore_index=-100)
+        else:
+            self.criterion = nn.CrossEntropyLoss(ignore_index=-100)
         self.optimizer = optimizer
         self.scheduler = scheduler
 
     def train(self, epochs: int, save_path, val_every: int = 1, accumulation_step: int = 1):
         assert epochs % val_every == 0, 'Epochs number should be divisible by \'val_every\' parameter'
         assert accumulation_step > 0, '\'accumulation_step\' parameter should be greater than zero'
+        os.makedirs(save_path, exist_ok=True)
         losses = []
         print('Start training')
         self.model.train()
@@ -40,11 +46,8 @@ class Trainer:
 
                 logits = self.model(**inputs).logits
                 num_targets = logits.shape[-1]
-                if not isinstance(self.criterion, DiceLoss):
-                    loss = self.criterion(logits.view(-1, num_targets), labels.flatten()) / accumulation_step
-                else:
-                    loss = self.criterion(logits.view(-1, num_targets), labels.flatten(),
-                                          mask=(labels.flatten() >= 0)) / accumulation_step
+                loss = self.criterion(logits.view(-1, num_targets), labels.flatten()) / accumulation_step
+
                 loss.backward()
 
                 mean_loss += loss.item() / len(self.train_dataloader)
